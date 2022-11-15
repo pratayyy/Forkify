466cb56adabd289ea28c8188bc84ed4a9a2a4af2
pratayyy <2028026@kiit.ac.in>
2022-11-15 20:30:22 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e5084cf9-b932-462c-9e4b-1321623874bb';
+export const MODAL_CLOSE_SEC = 2.5;
